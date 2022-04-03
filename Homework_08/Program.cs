220636c4e6f5c29c81ddde632cade036762921da
Forofonof <DanielResult1@outlook.com>
2022-04-03 20:24:58 +0300

    Добавьте файлы проекта.

---
+++ Homework_08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string userMessage;
+        bool wordOutputCheck = false;
+
+        while (!wordOutputCheck) 
+        {
+            Console.WriteLine("Для выхода из цикла, напишите: exit");
+            userMessage = Console.ReadLine();
+            if (userMessage == "exit")
+            {
+                wordOutputCheck = true;
+                break;
+            }
+        }
+        Console.WriteLine("Вы успешно вышли из цикла!");
+    }
+}
